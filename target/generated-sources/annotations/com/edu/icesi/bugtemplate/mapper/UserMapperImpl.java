44af3877d061e15bec24ff90ed7805838ef11f73
David Montaño <cuatro04sr@gmail.com>
2022-09-06 15:38:58 -0500

    fix: .idea

--- target/generated-sources/annotations/com/edu/icesi/bugtemplate/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/edu/icesi/bugtemplate/mapper/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-09-06T14:27:41-0500",
+    date = "2022-09-06T15:38:01-0500",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 11.0.15.1 (Oracle Corporation)"
 )
 @Component
